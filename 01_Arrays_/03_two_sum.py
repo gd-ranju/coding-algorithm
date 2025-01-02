b2fcc27f20f4ea29c35e865810785226b0c1507b
Niranjan <gdniranjangd@gmail.com>
2025-01-02 13:34:41 -0600

    Update 03_two_sum.py

--- 01_Arrays_/03_two_sum.py
+++ 01_Arrays_/03_two_sum.py
@@ -1,3 +1,11 @@
+# Time Complexity: O(n), where n is the number of elements in nums.
+# We iterate over the list once and each lookup/insertion operation in the dictionary takes constant time on average.
+
+# Space Complexity: O(n), where n is the number of elements in nums.
+# The dictionary 'prevMap' stores at most n elements in the worst case, corresponding to the number of numbers in the list.
+
+
+
 class Solution:
     def twoSum(self, nums, target: int) -> list[int]:
         prevMap = {}  # val -> index
